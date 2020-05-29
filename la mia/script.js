663b23ba15a4f21109f86ec41fb5ac484d156c1a
NicoGangi5 <1605558@ucc.edu.ar>
2020-05-29 13:14:26 -0300

    Fist commit

--- la mia/script.js
+++ la mia/script.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
     $('#btnToggle').on('click', function(e){
         let status;
         if($(this).text() == 'Rele Turn ON') {
-            $(this).text('Rele Turn OFF')
+            $(this).text('Rele Turn OFF');
+            $(this).removeClass().addClass('btn btn-block btn-light');
             status = 'on';
         } else {
             $(this).text('Rele Turn ON');
+            $(this).removeClass().addClass('btn btn-block btn-dark');
             status = 'off';
         }
         
